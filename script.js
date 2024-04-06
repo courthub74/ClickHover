14fcead584d5b158e57243dfe8f4adc5100e3707
Courdevelops <courdevelops@gmail.com>
2024-04-06 13:36:48 -0400

    try two eventlisteners

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let circle = document.getElementById('circle');
 
 circle.addEventListener('mouseover', function () {
     circle.classList.add('blue');
-    // Timer for Mobile
-    function reverse_circle() {
-        circle.classList.remove('blue');
-    }
-    setTimeout(reverse_circle, 3000);
+    // Timer for Mobile 
+    // function reverse_circle() {
+    //     circle.classList.remove('blue');
+    // }
+    // setTimeout(reverse_circle, 3000);
 });
 
 circle.addEventListener('mouseout', function () {
@@ -14,11 +14,16 @@ circle.addEventListener('mouseout', function () {
 });
 
 
-circle.addEventListener('click', function() {
-    circle.classList.add('blue');
-    // Timer for Mobile
-    function reverse_circle() {
-        circle.classList.remove('blue');
-    }
-    setTimeout(reverse_circle, 3000);
-});
+// function blue_button () {
+//     circle.classList.add('blue');
+//     // Timer for Mobile
+//     function reverse_circle() {
+//         circle.classList.remove('blue');
+//     }
+//     setTimeout(reverse_circle, 3000);
+// }
+
+// circle.addEventListener('click', function () {
+//     console.log("Clicked One");
+//     blue_button();
+// });
