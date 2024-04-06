e2ab04a743c83e998aaaab7adb65110698e0a270
Courdevelops <courdevelops@gmail.com>
2024-04-06 14:07:09 -0400

    pointerdowns applied

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 let circle = document.getElementById('circle');
 
-// circle.addEventListener('mouseover', () => {
-//     circle.classList.add('blue');
-//     // Timer for Mobile 
-//     // function reverse_circle() {
-//     //     circle.classList.remove('blue');
-//     // }
-//     // setTimeout(reverse_circle, 3000);
-// });
+// Desktop Interactions
+circle.addEventListener('mouseover', () => {
+    circle.classList.add('blue');
+    // Timer for Mobile 
+    // function reverse_circle() {
+    //     circle.classList.remove('blue');
+    // }
+    // setTimeout(reverse_circle, 3000);
+});
 
-// circle.addEventListener('mouseout',  () => {
-//     circle.classList.remove('blue');
-// });
+circle.addEventListener('mouseout',  () => {
+    circle.classList.remove('blue');
+});
 
-// Two Mouse Clicks
-circle.addEventListener('click', () => {
+// Pointerdown
+circle.addEventListener('pointerdown', e => {
+    console.log(e);
     console.log("Circle Clicked");
     circle.classList.add('blue');
     // Timer for Mobile
@@ -25,16 +27,4 @@ circle.addEventListener('click', () => {
 });
 
 
-// function blue_button () {
-//     circle.classList.add('blue');
-//     // Timer for Mobile
-//     function reverse_circle() {
-//         circle.classList.remove('blue');
-//     }
-//     setTimeout(reverse_circle, 3000);
-// }
 
-// circle.addEventListener('click', function () {
-//     console.log("Clicked One");
-//     blue_button();
-// });
